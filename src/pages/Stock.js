4b0cb0ae505ab884b008e7872effb24f144b6b01
Shania <shaniaantezana@gmail.com>
2023-01-02 15:29:47 -0400

    change stock.js

--- src/pages/Stock.js
+++ src/pages/Stock.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import Api from '../utils/api'
 export const Stock = () => {
+    const [products,setProducts] = useState()
+    useEffect(()=>{
+        const allProducts = async() =>{
+            try {
+              const products =  await Api.getProducts()
+              setProducts(products)
+            } catch (error) {
+              console.log(error)
+            }
+          }
+          allProducts()
+    },[])
   return (
     <div>
         <h1>Stock</h1>
+        <div></div>
     </div>
   )
 }
