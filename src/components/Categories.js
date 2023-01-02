df983a78fb8ea192c6fe57affbcafbf298c893ad
Shania <shaniaantezana@gmail.com>
2023-01-02 16:28:06 -0400

    insert component Category.js in Categories.js

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Category } from './Category'
 
 export const Categories = () => {
   return (
-    <div>Categories</div>
+    <div>
+      <Category/>
+    </div>
   )
 }
