ee660203289bb0092ebca73748f4e93e4c6da94d
Shania <shaniaantezana@gmail.com>
2023-01-02 15:05:16 -0400

    change Home.js and create Stock.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <button><Link to='/'>Stock</Link></button>
+    </div>
   )
 }
