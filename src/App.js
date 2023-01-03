f6037c2e578548673dfed28cdf1594f4b3cea378
Shania <shaniaantezana@gmail.com>
2023-01-03 16:30:18 -0400

    insert new route in App.js,create file NewProduct.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Home } from './pages/Home';
+import { NewProduct } from './pages/NewProduct';
 import { Stock } from './pages/Stock';
 
 function App() {
   return (
     <div className="App">
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/stock' element={<Stock/>} />
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/estoque' element={<Stock />} />
+        <Route path='/novoProduto' element={<NewProduct />} />
+      </Routes>
     </div>
   );
 }
