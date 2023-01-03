345d327d11622ea1d6d51a3747bb42e3f009bb8e
Shania <shaniaantezana@gmail.com>
2023-01-03 17:01:37 -0400

    delete AddProduct.js,NewProduct.js and change App.js ,Products.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Home } from './pages/Home';
-import { NewProduct } from './pages/NewProduct';
 import { Stock } from './pages/Stock';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/estoque' element={<Stock />} />
-        <Route path='/novoProduto' element={<NewProduct />} />
       </Routes>
     </div>
   );
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { Product } from './Product'
 
 export const Products = ({ products }) => {
   return (
     <div>
       {products.map((product) => <Product key={product._id} product={product} />)}
-      <button><Link to='/novoProduto'>Adicionar Produto</Link></button>
     </div>
   )
 }
