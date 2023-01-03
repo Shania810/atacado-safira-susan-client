c2297d590779874035edaee6f2c15a6a4640f005
Shania <shaniaantezana@gmail.com>
2023-01-03 10:21:16 -0400

    create function newCategory in AddCategory.js

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Api from '../utils/api'
 export const AddCategory = () => {
+  const [category,setCategory] = useState('')
+  const newCategory = async (e) =>{
+    e.preventDefault()
+    try {
+      await Api.postCategory({category: category})
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <div>
-        <input type='text'/>
-    </div>
+    <form onSubmit={(e)=>newCategory(e)}>
+        <input type='text' value={category} onChange={(e)=>setCategory(e.target.value)}/>
+    </form>
   )
 }
