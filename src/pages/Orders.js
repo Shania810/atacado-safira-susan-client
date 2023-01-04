dbbef5e48ce02619ef4bba9fd1e45e773f8ebdb3
Shania <shaniaantezana@gmail.com>
2023-01-04 16:36:04 -0400

    create state orders,function allOrders

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import Api from '../utils/api'
+import {Order} from '../components/Order'
 export const Orders = () => {
+  const [orders,setOrders] = useState([])
+  const allOrders = async() =>{
+    try {
+      const orders = await Api.getOrders()
+      setOrders(orders)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+   allOrders()
+  },[])
   return (
-    <div>Orders</div>
+    <div>
+      <h1>Pedidos</h1>
+      {orders.map((order)=> <Order/>)}
+    </div>
   )
 }
