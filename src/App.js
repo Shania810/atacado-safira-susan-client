5442ceb05da86b9d2ee98ed255db4c12f742a62f
Shania <shaniaantezana@gmail.com>
2023-01-04 16:15:58 -0400

    create route Orders in App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Home } from './pages/Home';
 import { NewProduct } from './pages/NewProduct';
 import { Product } from './pages/Product';
 import { Stock } from './pages/Stock';
+import { Orders } from './pages/Orders';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/estoque' element={<Stock />} />
         <Route path='/produto/:id' element={<Product />} />
         <Route path='/novoProduto' element={<NewProduct />} />
+        <Route path='/pedidos' element={<Orders />} />
       </Routes>
     </div>
   );
