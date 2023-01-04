eb077cc612720b2196d5da24b498383ed480973e
Shania <shaniaantezana@gmail.com>
2023-01-04 17:10:52 -0400

    insert Link in OrderCard.js

--- src/components/OrderCard.js
+++ src/components/OrderCard.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export const OrderCard = ({order,number}) => {
-  const {total,seller} = order
+export const OrderCard = ({ order, number }) => {
+  const { _id, total, seller } = order
   return (
     <div>
-      <div>Pedido {number + 1}</div>
-      <div>Vendedor {seller}</div>
-      <div>Total {total}</div>
+      <Link to={`/pedido/${_id}`}>
+        <div>Pedido {number + 1}</div>
+        <div>Vendedor {seller}</div>
+        <div>Total {total}</div>
+      </Link>
     </div>
   )
 }
