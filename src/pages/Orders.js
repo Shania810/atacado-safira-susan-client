d1018337c209ffa60254e6959d3aeeffe28b4411
Shania <shaniaantezana@gmail.com>
2023-01-04 20:33:50 -0400

    insert button in page Orders.js

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Api from '../utils/api'
 import {OrderCard} from '../components/OrderCard'
+import { Link } from 'react-router-dom'
 export const Orders = () => {
   const [orders,setOrders] = useState([])
   const allOrders = async() =>{
@@ -18,6 +19,7 @@ export const Orders = () => {
     <div>
       <h1>Pedidos</h1>
       {orders.map((order,index)=> <OrderCard order={order} number={index}/>)}
+      <button><Link to='/novoPedido'>Nova venda</Link></button>
     </div>
   )
 }
