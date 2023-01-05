6f84fdc1425425f1c408a848a9a6a2a120ebe5f6
Shania <shaniaantezana@gmail.com>
2023-01-04 20:38:04 -0400

    insert AddOrder.js in NewOrder.js

--- src/pages/NewOrder.js
+++ src/pages/NewOrder.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { AddOrder } from '../components/AddOrder'
 
 export const NewOrder = () => {
-  return (
-    <div>NewOrder</div>
-  )
+    return (
+        <div>
+            <h1>Novo Pedido</h1>
+            <AddOrder />
+        </div>
+    )
 }
