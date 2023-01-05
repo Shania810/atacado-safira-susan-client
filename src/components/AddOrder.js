64cff8a0d88db761be844713d3785ed806104a3f
Shania <shaniaantezana@gmail.com>
2023-01-05 15:16:56 -0400

    change AddOrder.js

--- src/components/AddOrder.js
+++ src/components/AddOrder.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import {Search} from './Search'
 import Api from '../utils/api'
+import { Order } from './Order'
 export const AddOrder = () => {
   const [products,setProducts] = useState([])
   const [search,setSearch] = useState('')
+  const [orderItems,setOrderItems] = useState([])
   const allProducts = async() =>{
     try {
       const products = await Api.getProducts()
@@ -24,16 +26,37 @@ export const AddOrder = () => {
       console.log(error)
     }
   }
+  const addOrderItem = (product) =>{
+   const validate = orderItems.filter((orderItem)=> orderItem.name === product.name )
+   if(validate.length >= 2){
+    console.log('já existe')
+    }else{
+      setOrderItems([...orderItems,product])
+    }
+  }
   return (
     <div>
      <Search search={search} searchProduct={searchProduct}/>
       {products?.map((product)=>{
-        return <div key={product._id} >
+        return <div key={product._id} onClick={()=>addOrderItem(product)}>
           <div>{product.name}</div>
           <div>{product.stock}</div>
           <div>{product.category.name}</div>
         </div>
       })}
+      <table>
+      <thead>
+        <td>
+          <tr>Item</tr>
+          <tr>Produto</tr>
+          <tr>Quantidade</tr>
+          <tr>Preço</tr>
+        </td>
+      </thead>
+      <tbody>
+      {orderItems.map((orderItem)=> <Order orderItem={orderItem} /> )}
+      </tbody>
+      </table>
     </div>
   )
 }
