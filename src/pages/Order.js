435826183a7d588411429d0e6e00e391d3198f01
Shania <shaniaantezana@gmail.com>
2023-01-06 15:16:02 -0400

    create function deleteOrder and insert button deleteOrder in Order.js

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,56 +1,65 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { PrintComponent } from '../components/PrintComponent'
 import Api from '../utils/api'
 export const Order = () => {
-  const {id} = useParams() 
-  const [order,setOrder] = useState({})
-  
-  const getOrder = async(id) =>{
+  const { id } = useParams()
+  const [order, setOrder] = useState({})
+  const navigate = useNavigate()
+  const getOrder = async (id) => {
     try {
-        const order = await Api.getOrder(id)
-        setOrder(order)
-        
+      const order = await Api.getOrder(id)
+      setOrder(order)
+
     } catch (error) {
-        console.log(error)
+      console.log(error)
     }
   }
-  useEffect(()=>{
+  useEffect(() => {
     getOrder(id)
-  },[id])
+  }, [id])
+  const deleteOrder = async (id) => {
+    try {
+      await Api.deleteOrder(id)
+      navigate('/pedidos')
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div>
-       <div>Vendedor(a): {order?.seller}</div>
-       <table>
-       <thead>
-       <tr>
+      <div>Vendedor(a): {order?.seller}</div>
+      <table>
+        <thead>
+          <tr>
             <td>Item</td>
             <td>Produto</td>
             <td>Preço</td>
             <td>Quantidade</td>
             <td>Total</td>
-        </tr>
-       </thead>
-       <tbody>
-        {order?.order_items?.map((item,index)=>{
-          return <tr key={item._id} >
-            <td>{index + 1}</td>
-            <td>{item.product.name}</td>
-            <td>{item.quantity < 6 ? item.product.retail_price : item.product.wholesale_price }</td>
-            <td>{item.quantity}</td> 
-            <td>{item.total}</td>
           </tr>
-        })}
-        <tr>
-          <td></td>
+        </thead>
+        <tbody>
+          {order?.order_items?.map((item, index) => {
+            return <tr key={item._id} >
+              <td>{index + 1}</td>
+              <td>{item.product.name}</td>
+              <td>{item.quantity < 6 ? item.product.retail_price : item.product.wholesale_price}</td>
+              <td>{item.quantity}</td>
+              <td>{item.total}</td>
+            </tr>
+          })}
+          <tr>
+            <td></td>
             <td></td>
             <td></td>
             <td></td>
             <td>{order?.total}</td>
-        </tr>
+          </tr>
         </tbody>
-       </table>
-       <PrintComponent order={order} />
+      </table>
+      <PrintComponent order={order} />
+      <button onClick={() => deleteOrder(order._id)} >Excluir Pedido</button>
     </div>
   )
 }
