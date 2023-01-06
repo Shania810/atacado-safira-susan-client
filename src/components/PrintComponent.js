89d062524ec283d264e310b7c74e19b79ee18a68
Shania <shaniaantezana@gmail.com>
2023-01-06 10:26:37 -0400

    insert component Print.js in PrintComponent.js

--- src/components/PrintComponent.js
+++ src/components/PrintComponent.js
@@ -1,10 +1,12 @@
 import React, { useRef } from 'react'
 import ReactToPrint from 'react-to-print'
+import {Print} from './Print'
 export const PrintComponent = () => {
   let componentRef = useRef()
   return (
     <div>
       <ReactToPrint trigger={()=><button>Imprimir</button>} content={()=> componentRef}/>
+      <Print ref={(el) => (componentRef = el)} />
     </div>
   )
 }
