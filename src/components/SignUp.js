96f145187ba236b311c5685971eb85a27e09bac7
Shania <shaniaantezana@gmail.com>
2023-01-07 17:12:07 -0400

    change component SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Api from '../utils/api'
 
 export const SignUp = () => {
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const newUser = async (e) => {
+    e.preventDefault()
+    const user = {
+      name,
+      password
+    }
+    try {
+      await Api.signUp(user)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <div>SignUp</div>
+    <form onSubmit={(e) => newUser(e)} >
+      <h1>Criar Conta</h1>
+      <div>
+        <label>Nome do Usuário</label>
+        <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div>
+        <label>Senha do Usuário</label>
+        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button type='submit'>Criar Usuário</button>
+    </form>
   )
 }
