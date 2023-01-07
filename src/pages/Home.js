b70df8dbc6342dbf892a8aefc2124b90c96e4646
Shania <shaniaantezana@gmail.com>
2023-01-06 20:54:30 -0400

    insert button in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ export const Home = () => {
         <h1>Atacado Safira Susan</h1>
         <button><Link to='/estoque'>Estoque</Link></button>
         <button><Link to='/pedidos'>Pedidos</Link></button>
+        <button><Link to='/vendedores'>Vendedores</Link></button>
     </div>
   )
 }
