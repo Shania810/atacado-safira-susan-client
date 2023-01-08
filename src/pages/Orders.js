3b01a2b6c644ad06ca247525a4a5c47c99f3fe17
Shania <shaniaantezana@gmail.com>
2023-01-08 05:42:16 -0400

    change Orders.js

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import Api from '../utils/api'
-import {OrderCard} from '../components/OrderCard'
+import { OrderCard } from '../components/OrderCard'
 import { Link } from 'react-router-dom'
+import { NoUser } from './NoUser'
 export const Orders = () => {
-  const [orders,setOrders] = useState([])
-  const allOrders = async() =>{
+  const user = localStorage.getItem('token')
+  const [orders, setOrders] = useState([])
+  const allOrders = async () => {
     try {
       const orders = await Api.getOrders()
       setOrders(orders)
@@ -12,14 +14,18 @@ export const Orders = () => {
       console.log(error)
     }
   }
-  useEffect(()=>{
-   allOrders()
-  },[])
-  return (
-    <div>
-      <h1>Pedidos</h1>
-      {orders.map((order,index,items)=> <OrderCard key={order._id} order={order} number={index} items={items}/>)}
-      <button><Link to='/novoPedido'>Nova venda</Link></button>
-    </div>
-  )
+  useEffect(() => {
+    allOrders()
+  }, [])
+  if (user) {
+    return (
+      <div>
+        <h1>Pedidos</h1>
+        {orders.map((order, index, items) => <OrderCard key={order._id} order={order} number={index} items={items} />)}
+        <button><Link to='/novoPedido'>Nova venda</Link></button>
+      </div>
+    )
+  } else {
+    return <NoUser />
+  }
 }
