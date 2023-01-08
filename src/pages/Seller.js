df4a0ef04b50f667bbf82f30fc43ea7d1c7be8d4
Shania <shaniaantezana@gmail.com>
2023-01-08 02:21:47 -0400

    change Seller.js

--- src/pages/Seller.js
+++ src/pages/Seller.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Api from '../utils/api'
 
 export const Seller = () => {
+  const {id} = useParams()
+  const [seller,setSeller] = useState({})
+
+  useEffect(()=>{
+    const getSeller = async(idSeller) =>{
+      try {
+        const seller = await Api.getSeller(idSeller)
+        setSeller(seller)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getSeller(id)
+  },[id])
+  console.log(seller)
   return (
-    <div>Seller</div>
+    <div>
+      <h1>Vendedor(a){seller.name}</h1>
+      {seller?.orders?.map((order)=>{
+        return <div key={order._id} >
+          <h2>{order.seller.name}</h2>
+        </div>
+      })}
+    </div>
   )
 }
