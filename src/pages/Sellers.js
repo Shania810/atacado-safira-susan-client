2b0d23f24f4ad773f3cf70682fe217c0d307309a
Shania <shaniaantezana@gmail.com>
2023-01-08 00:09:46 -0400

    change Sellers.js

--- src/pages/Sellers.js
+++ src/pages/Sellers.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Api from '../utils/api'
 
 export const Sellers = () => {
+  const [sellers,setSellers] = useState([])
+  useEffect(()=>{
+    const allSellers = async()=>{
+      try {
+        const sellers = await Api.getSellers()
+        setSellers(sellers)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    allSellers()
+  },[])
   return (
-    <div>Sellers</div>
+    <div>
+      <h1>Vendedores</h1>
+      {sellers.map(({name})=>{
+        return <div>
+          <h1>Vendedor(a){name}</h1>
+          <button>Mais detalhes</button>
+        </div>
+      })}
+    </div>
   )
 }
