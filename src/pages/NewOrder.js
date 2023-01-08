1ea0a01e57aafba849a7d6a75c706110e800b32a
Shania <shaniaantezana@gmail.com>
2023-01-08 05:30:49 -0400

    change NewOrder.js and NewProduct.js

--- src/pages/NewOrder.js
+++ src/pages/NewOrder.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { AddOrder } from '../components/AddOrder'
+import { NoUser } from './NoUser'
 
 export const NewOrder = () => {
-    return (
-        <div>
-            <h1>Novo Pedido</h1>
-            <AddOrder />
-        </div>
-    )
+    const user = localStorage.getItem('token')
+    if (user) {
+        return (
+            <div>
+                <h1>Novo Pedido</h1>
+                <AddOrder />
+            </div>
+        )
+    } else {
+        return <NoUser />
+    }
 }
--- src/pages/NewProduct.js
+++ src/pages/NewProduct.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { AddProduct } from '../components/AddProduct'
+import { NoUser } from './NoUser'
 
 export const NewProduct = () => {
-    return (
-        <AddProduct />
-    )
+    const user = localStorage.getItem('token')
+    if (user) {
+        return <AddProduct />
+    } else {
+        return <NoUser />
+    }
 }
