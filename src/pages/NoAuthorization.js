69fa8011add9123dbe6dba82702e6c23cb91c125
Shania <shaniaantezana@gmail.com>
2023-01-08 05:19:33 -0400

    change NoAuthorization.js

--- src/pages/NoAuthorization.js
+++ src/pages/NoAuthorization.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const NoAuthorization = () => {
   return (
-    <div>NoAuthorization</div>
+    <div>
+      <h1>Não autorizado</h1>
+      <h2>Cria uma conta ou acesse sua conta</h2>
+    </div>
   )
 }
