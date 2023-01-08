b5b2eaf49a1fe9c554b8a4e4d6932970547c568d
Shania <shaniaantezana@gmail.com>
2023-01-07 20:26:49 -0400

    change LogIn.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Api from '../utils/api'
 
 export const LogIn = () => {
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  const verifyUser = async (e) => {
+    e.preventDefault()
+    const user = {
+      name,
+      password
+    }
+    try {
+      await Api.logIn(user)
+      navigate('/pedidos')
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <div>Login</div>
+    <form onSubmit={(e) => verifyUser(e)} >
+      <div>
+        <label>Nome de usuário</label>
+        <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div>
+        <label>Senha do usuário</label>
+        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button type='submit'>Fazer Login</button>
+    </form>
   )
 }
