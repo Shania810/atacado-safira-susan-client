bd5bfe4d566f183b48200f95af72e23cb47f2462
Shania <shaniaantezana@gmail.com>
2023-01-09 21:10:45 -0400

    change profits.js

--- src/pages/Profits.js
+++ src/pages/Profits.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Api from '../utils/api'
 
 export const Profits = () => {
+  const [profits,setProfits] = useState([])
+  useEffect(()=>{
+    const allProfits = async()=>{
+      try {
+        const profits = await Api.getProfits()
+        setProfits(profits)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    allProfits()
+  },[])
   return (
-    <div>Profits</div>
+    <div>
+      <h1>Lucros</h1>
+    </div>
   )
 }
