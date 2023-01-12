9c8384f8fa4b722f0202cc0d9e49e0425f138922
Shania <shaniaantezana@gmail.com>
2023-01-12 18:12:10 -0400

    create new route in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import {NavBar} from './components/NavBar'
 import { Home } from './pages/Home';
 import { NewProduct } from './pages/NewProduct';
 import { Product } from './pages/Product';
@@ -17,6 +18,7 @@ import { Profits } from './pages/Profits';
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/estoque' element={<Stock />} />
