0df64654bce1f9bf7ca71f174e6effa0f9bac109
Shania <shaniaantezana@gmail.com>
2023-01-13 11:46:41 -0400

    change NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,13 +9,13 @@ export const NavBar = () => {
     <div>
       <IconMenu onClick={() => setValue(!value)}><GiHamburgerMenu /></IconMenu>
       {value && <Nav>
-        {!user && <NavLink to='/criarConta'>Criar conta</NavLink>}
-        {!user && <NavLink to='/acessarConta'>Acessar conta</NavLink>}
-        {user && <NavLink to='/estoque'>Estoque</NavLink>}
-        {user && <NavLink to='/pedidos'>Pedidos</NavLink>}
-        {typeUser === 'admin' && <NavLink to='/vendedores'>Vendedores</NavLink>}
-        {user && <NavLink to='/clientes'>Clientes</NavLink>}
-        {typeUser === 'admin' && <NavLink to='/lucros'>Lucros</NavLink>}
+        {!user && <NavLink onClick={() => setValue(false)}  to='/criarConta'>Criar conta</NavLink>}
+        {!user && <NavLink onClick={() => setValue(false)}  to='/acessarConta'>Acessar conta</NavLink>}
+        {user && <NavLink onClick={() => setValue(false)} to='/estoque'>Estoque</NavLink>}
+        {user && <NavLink onClick={() => setValue(false)} to='/pedidos'>Pedidos</NavLink>}
+        {typeUser === 'admin' && <NavLink onClick={() => setValue(false)} to='/vendedores'>Vendedores</NavLink>}
+        {user && <NavLink onClick={() => setValue(false)} to='/clientes'>Clientes</NavLink>}
+        {typeUser === 'admin' && <NavLink onClick={() => setValue(false)} to='/lucros'>Lucros</NavLink>}
       </Nav>}
     </div>
   )
