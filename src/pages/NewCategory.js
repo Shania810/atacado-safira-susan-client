01569e6027e2d408d5cb3a33848bb62a03bd506f
Shania <shaniaantezana@gmail.com>
2023-02-20 14:10:05 -0400

    changed file NewCategory.js

--- src/pages/NewCategory.js
+++ src/pages/NewCategory.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import { useState } from 'react'
-import { Button, Container, Form, Input, Label, LabelInput } from '../design/styled.components'
+import { AddCategory } from '../components/AddCategory'
+import { Container } from '../design/styled.components'
 
 export const NewCategory = () => {
-  const [category, setCategory] = useState('')
   return (
-    <Container alignItems='center'>
-        <Form>
-           <LabelInput>
-            <Label>Nova Categoria</Label>
-            <Input type='text' value={category} onChange={(e)=> setCategory(e.target.value)} />
-           </LabelInput>
-           <Button>Adicionar Categoria</Button>
-        </Form>
+    <Container>
+      <AddCategory/>
     </Container>
   )
 }
