b98f1498104c9e6ec6e73c540f33f03073a8e69a
Shania <shaniaantezana@gmail.com>
2023-02-20 14:15:17 -0400

    changed component AddCategory.js

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,19 +1,29 @@
 import React, { useState } from 'react'
 import Api from '../utils/api'
-export const AddCategory = ({allCategories}) => {
+import { Button, Container, Form, Input, Label, LabelInput } from '../design/styled.components'
+import { useNavigate } from 'react-router-dom'
+
+export const AddCategory = () => {
   const [category,setCategory] = useState('')
+  const navigate = useNavigate('')
   const newCategory = async (e) =>{
     e.preventDefault()
     try {
       await Api.postCategory({name: category})
-      allCategories()
+      navigate('/estoque')
     } catch (error) {
       console.log(error)
     }
   }
   return (
-    <form onSubmit={(e)=>newCategory(e)}>
-        <input type='text' value={category} onChange={(e)=>setCategory(e.target.value)}/>
-    </form>
+    <Container alignItems='center'>
+        <Form onSubmit={(e)=>newCategory(e)} >
+           <LabelInput>
+            <Label>Nova Categoria</Label>
+            <Input type='text' value={category} onChange={(e)=> setCategory(e.target.value)} />
+           </LabelInput>
+           <Button>Adicionar Categoria</Button>
+        </Form>
+    </Container>
   )
 }
